54dd26a6043f0ae4e5fa07bcf1eec3c29a449cfb
tomik3690 <tomik031195@gmail.com>
2016-12-20 12:02:06 +0100

    init

---
+++ app/src/main/java/com/example/u410/weather/CurrentWeather.java
@@ -0,0 +1,8 @@
+package com.example.u410.weather;
+
+/**
+ * Created by U410 on 2016-12-20.
+ */
+
+public class CurrentWeather {
+}
---
+++ app/src/main/java/com/example/u410/weather/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.u410.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            endpoint_ = new URL("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=" + API_KEY);
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final String API_KEY = "41fae9dcd05e0fcbf176197ce26531e2";
+
+    private URL endpoint_;
+}
