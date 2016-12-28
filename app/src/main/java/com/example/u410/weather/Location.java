5d270d01d4398a370518b0b120eaa4d860e455c4
kkryus <kamilkryus@gmail.com>
2016-12-28 12:52:40 +0100

    added finding localization

--- app/src/main/java/com/example/u410/weather/Location.java
+++ app/src/main/java/com/example/u410/weather/Location.java
@@ -1,8 +1,82 @@
 package com.example.u410.weather;
 
-/**
- * Created by U410 on 2016-12-20.
- */
 
-public class Location {
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import static android.content.Context.LOCATION_SERVICE;
+
+public class Location
+{
+    private Context ctx;
+    private String cityName, addressName, countryName, postalCode;
+    private LocationManager mLocationManager;
+    private android.location.Location lastKnownLocation;
+    private Double latitude, longitude;
+
+    public Location(Context context)
+    {
+        ctx = context;
+        mLocationManager = (LocationManager) ctx.getSystemService(LOCATION_SERVICE);
+    }
+
+    public String getCityName(){ return cityName; }
+    public String getAddressName(){ return addressName; }
+    public String getCountryName() { return countryName; }
+    public String getPostalCode() { return postalCode; }
+
+    public void assignPlace()
+    {
+        findCoordinates();
+        findPlace();
+    }
+
+    public void findCoordinates()
+    {
+        try
+        {
+            lastKnownLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if(lastKnownLocation!=null)
+            {
+                latitude = lastKnownLocation.getLatitude();
+                longitude = lastKnownLocation.getLongitude();
+            }
+            else
+            {
+                latitude = 0.0;
+                longitude = 0.0;
+            }
+        }
+       catch(SecurityException e)
+       {
+
+       }
+    }
+
+    public void findPlace()
+    {
+        try
+        {
+            Geocoder geocoder = new Geocoder(ctx, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+            cityName = addresses.get(0).getLocality();
+            addressName = addresses.get(0).getAddressLine(0);
+            countryName = addresses.get(0).getCountryName();
+            postalCode = addresses.get(0).getPostalCode();
+        }
+        catch(IOException e)
+        {
+
+        }
+    }
+
+    public void destroyContext()
+    {
+        ctx = null;
+    }
 }
